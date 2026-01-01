eb9509528bd5a5e0e6cd318ba4824bc4651e2ae0
JavCombita <dcmz97@gmail.com>
2026-01-01 18:31:59 -0400

    Update GhostBehavior.cs

--- Entities/Behaviors/GhostBehavior.cs
+++ Entities/Behaviors/GhostBehavior.cs
@@ -12,41 +12,42 @@ namespace MonstrosityFramework.Entities.Behaviors
         {
             monster.isGlider.Value = true;
             monster.HideShadow = true;
-            monster.Slipperiness = 8; // Ghost Vanilla es muy resbaladizo
+            monster.Slipperiness = 8; 
 
-            // Generar ID único para la luz (basado en posición inicial)
-            int lightId = (int)monster.Position.X * 1000 + (int)monster.Position.Y + Game1.random.Next(100);
-            monster.SetVar("lightId", lightId);
+            // Generamos ID único como string para 1.6
+            string lightId = monster.MonsterSourceId.Value + "_" + monster.Position.ToString();
+            monster.SetObj("lightId", lightId);
         }
 
         public override void OnUpdateAnimation(CustomMonster monster, GameTime time)
         {
-            // Lógica de Iluminación (Configurable por JSON)
             if (GetCustomInt(monster, "HasLight", 0) == 1)
             {
-                int id = (int)monster.GetVar("lightId");
-                // Leemos color del JSON o usamos un amarillo verdoso por defecto
+                string id = monster.GetObj<string>("lightId");
                 Color lightColor = GetCustomColor(monster, "LightColor", new Color(64, 255, 64));
 
-                LightSource light = Game1.currentLightSources.FirstOrDefault(l => l.identifier == id);
-                if (light == null)
+                // Lógica de Luces para Stardew Valley 1.6
+                // currentLightSources es un Dictionary<string, LightSource>
+                if (!Game1.currentLightSources.ContainsKey(id))
                 {
-                    light = new LightSource(4, monster.Position, 1.5f, lightColor, id, LightSource.LightContext.None, 0L);
-                    Game1.currentLightSources.Add(light);
+                    // Constructor 1.6: LightSource(string Id, int TextureIndex, Vector2 Position, float Radius, Color Color, ...)
+                    // TextureIndex 4 = Luz circular
+                    Game1.currentLightSources.Add(id, new LightSource(id, 4, monster.Position + new Vector2(32f, 32f), 1.5f, lightColor));
                 }
-                
-                // Actualizar posición de la luz
-                light.position.Value = monster.Position + new Vector2(32f, 32f);
+                else
+                {
+                    // Actualizar posición
+                    Game1.currentLightSources[id].position.Value = monster.Position + new Vector2(32f, 32f);
+                }
             }
         }
 
         public override void OnDeath(CustomMonster monster)
         {
-            // Limpiar luz
             if (GetCustomInt(monster, "HasLight", 0) == 1)
             {
-                int id = (int)monster.GetVar("lightId");
-                Utility.removeLightSource(id);
+                string id = monster.GetObj<string>("lightId");
+                if (id != null) Game1.currentLightSources.Remove(id);
             }
         }
 
@@ -54,17 +55,15 @@ namespace MonstrosityFramework.Entities.Behaviors
         {
             monster.isGlider.Value = true;
 
-            // Oscilación visual (Ghost.cs) -> yOffset
             float yOffset = (float)Math.Sin(time.TotalGameTime.TotalMilliseconds / 1000.0 * (Math.PI * 2.0)) * 20f;
             monster.drawOffset = new Vector2(0, yOffset);
 
             if (monster.Player != null)
             {
-                // Cálculo de trayectoria suavizada
                 Point pDiff = monster.Player.StandingPixel - monster.StandingPixel;
                 float xSlope = -pDiff.X;
                 float ySlope = pDiff.Y;
-                float t = 400f; // Constante de inercia
+                float t = 400f; 
                 xSlope /= t; 
                 ySlope /= t;
 
@@ -73,14 +72,12 @@ namespace MonstrosityFramework.Entities.Behaviors
                 monster.xVelocity += (-xSlope * maxAccel / 6f);
                 monster.yVelocity += (-ySlope * maxAccel / 6f);
 
-                // Fricción
                 if (Math.Abs(monster.xVelocity) > Math.Abs(-xSlope * 5f)) 
                     monster.xVelocity -= (-xSlope * maxAccel / 6f);
                 if (Math.Abs(monster.yVelocity) > Math.Abs(-ySlope * 5f)) 
                     monster.yVelocity -= (-ySlope * maxAccel / 6f);
             }
 
-            // Orientación basada en velocidad mayor
             if (Math.Abs(monster.xVelocity) > Math.Abs(monster.yVelocity))
                 monster.FacingDirection = monster.xVelocity > 0 ? 1 : 3;
             else
@@ -91,7 +88,6 @@ namespace MonstrosityFramework.Entities.Behaviors
 
         public override int OnTakeDamage(CustomMonster monster, int damage, bool isBomb, Farmer who)
         {
-            // Efecto visual y empuje extra al ser golpeado
             Utility.addSprinklesToLocation(monster.currentLocation, monster.TilePoint.X, monster.TilePoint.Y, 2, 2, 101, 50, Color.LightBlue);
             
             monster.xVelocity = -monster.xVelocity * 1.5f;
