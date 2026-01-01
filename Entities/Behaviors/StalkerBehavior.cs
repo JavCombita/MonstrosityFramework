96669e57cf564457ddb5efeb2fe9dac7aa86b7be
JavCombita <dcmz97@gmail.com>
2026-01-01 17:17:59 -0400

    Refactor player detection and animation in StalkerBehavior
    Refactor StalkerBehavior to improve player detection logic and animation handling.

--- Entities/Behaviors/StalkerBehavior.cs
+++ Entities/Behaviors/StalkerBehavior.cs
@@ -7,21 +7,29 @@ namespace MonstrosityFramework.Entities.Behaviors
     {
         public override void Update(CustomMonster monster, GameTime time)
         {
-            float detection = GetVisionRange(monster, 16f);
+            // Usamos el Helper GetVisionRange que lee "DetectionRange" del JSON
+            float range = GetVisionRange(monster, 16); 
 
-            if (!IsPlayerWithinRange(monster, detection))
+            if (monster.Player != null && IsPlayerWithinRange(monster, range))
             {
+                monster.IsWalkingTowardPlayer = true;
+                
+                // Forzar umbral de movimiento
+                monster.moveTowardPlayerThreshold.Value = (int)range; 
+                
+                if (monster.isMoving()) 
+                {
+                    // Animación estándar (Arriba, Abajo, Izq, Der automáticos por Stardew)
+                    monster.Sprite.AnimateDown(time);
+                }
+            }
+            else
+            {
+                // Idle
                 monster.IsWalkingTowardPlayer = false;
                 monster.Halt();
-                monster.Sprite.currentFrame = monster.FacingDirection * 4;
-                return;
+                monster.Sprite.StopAnimation();
             }
-
-            monster.IsWalkingTowardPlayer = true;
-            monster.moveTowardPlayer(monster.Speed);
-
-            int startFrame = monster.FacingDirection * 4;
-            monster.Sprite.Animate(time, startFrame, 4, 150f);
         }
     }
 }
